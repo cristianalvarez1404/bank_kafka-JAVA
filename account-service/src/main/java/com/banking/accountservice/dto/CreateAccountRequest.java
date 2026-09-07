f22f4b2272ff53c1cec3ae72a557aaa8e4855498
cristiann <cristianandres1496@gmail.com>
2026-09-06 19:38:07 -0500

    account entity

---
+++ account-service/src/main/java/com/banking/accountservice/dto/CreateAccountRequest.java
@@ -0,0 +1,12 @@
+package com.banking.accountservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateAccountRequest {
+
+}
