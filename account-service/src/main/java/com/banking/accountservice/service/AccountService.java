02c3bbf22740817cde595eff357f2a5453281047
cristiann <cristianandres1496@gmail.com>
2026-09-11 17:02:24 -0500

    account event consumer config

--- account-service/src/main/java/com/banking/accountservice/service/AccountService.java
+++ account-service/src/main/java/com/banking/accountservice/service/AccountService.java
@@ -50,6 +50,101 @@ public class AccountService {
         return mapToResponse(savedAccount);
     }
 
+    /*
+     * Get account by account number
+     * @param accountNumber
+     * @return
+     * */
+    public AccountResponse getAccount(String accountNumber){
+        Account account = accountRepository.findAccountByAccountNumber(accountNumber)
+                .orElseThrow(() -> new RuntimeException("Account not found"));
+
+        return mapToResponse(account);
+    }
+
+    /*
+    * Get account balance
+    * @param accountNumber
+    * @return
+    * */
+    public BigDecimal getBalance(String accountNumber){
+        Account account = accountRepository.findAccountByAccountNumber(accountNumber)
+                .orElseThrow(() -> new RuntimeException("Account not found"));
+
+        return account.getBalance();
+    }
+
+    /*
+    * Block account - called by Fraud detection service via kafka
+    * @param accountNumber
+    */
+
+    /*
+    *   Block account - called by fraud detection service via kafka
+    * */
+    public void blockAccount(String accountNumber){
+        log.info("Blocking account: {}", accountNumber);
+        Account account = accountRepository.findAccountByAccountNumber(accountNumber)
+                .orElseThrow(() -> new RuntimeException("Account not found"));
+
+        account.setAccountStatus(AccountStatus.BLOCKED);
+        accountRepository.save(account);
+        log.info("Account blocked: {}", accountNumber);
+    }
+
+    public void unblockAccount(String accountNumber){
+        log.info("unblocking account: {}", accountNumber);
+        Account account = accountRepository.findAccountByAccountNumber(accountNumber)
+                .orElseThrow(() -> new RuntimeException("Account not found"));
+
+        account.setAccountStatus(AccountStatus.ACTIVE);
+        accountRepository.save(account);
+        log.info("Account unblocked: {}", accountNumber);
+    }
+
+    /*
+    * Deduct balance from sender account.
+    * Called by Transaction Service
+    * @param accountNumber
+    * @param amount
+    */
+    public void deductBalance(String accountNumber, BigDecimal amount){
+        log.info("Deduction balance {} from account: {}", amount, accountNumber);
+        Account account = accountRepository.findAccountByAccountNumber(accountNumber)
+                .orElseThrow(() -> new RuntimeException("Account not found"));
+        if(account.getAccountStatus() != AccountStatus.ACTIVE){
+            throw new RuntimeException("Account is not active " + accountNumber);
+        }
+
+        if(account.getBalance().compareTo(amount) < 0){
+            throw new RuntimeException("Insufficient funds for account " + accountNumber);
+        }
+
+        account.setBalance(account.getBalance().subtract(amount));
+        accountRepository.save(account);
+
+        log.info("Balance updated. New Balance: {}", account.getBalance());
+    }
+
+    /*
+    * Credit balance
+    * Called by Transaction Service via kafka
+    * @Param accountNumber
+    * @Param amount
+    * */
+    public void creditBalance(String accountNumber, BigDecimal  amount){
+        log.info("Crediting {} to account: {}", amount, accountNumber);
+
+        Account account = accountRepository.findAccountByAccountNumber(accountNumber)
+                .orElseThrow(() -> new RuntimeException("Account not found"));
+
+        account.setBalance(account.getBalance().add(amount));
+        accountRepository.save(account);
+
+        log.info("Balance Credited. New Balance: {}", account.getBalance());
+    }
+
+    // Generate unique 12 digit account number
     private String generateAccountNumber() {
         String accountNumber;
 
@@ -74,25 +169,5 @@ public class AccountService {
         response.setCreatedAt(account.getCreatedAt());
 
         return response;
-    }
-
-    public AccountResponse getAccount(String accountNumber){
-        return null;
-    }
-
-    public BigDecimal getBalance(String accountNumber){
-        return null;
-    }
-
-    public void blockAccount(String accountNumber){
-        return ;
-    }
-
-    public void deductBalance(String accountNumber, BigDecimal amount){
-        return;
-    }
-
-    public void creditBalance(String accountNumber, BigDecimal  amount){
-        return;
     }
 }
