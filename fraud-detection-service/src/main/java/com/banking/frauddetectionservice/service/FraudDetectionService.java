663f938389eaa5f4479e70edc6a155dff7d155ae
cristiann <cristianandres1496@gmail.com>
2026-09-16 18:35:23 -0500

    fraud service

---
+++ fraud-detection-service/src/main/java/com/banking/frauddetectionservice/service/FraudDetectionService.java
@@ -0,0 +1,32 @@
+package com.banking.frauddetectionservice.service;
+
+import com.banking.frauddetectionservice.client.AccountServiceClient;
+import com.banking.frauddetectionservice.model.FraudCheckResult;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class FraudDetectionService {
+
+    private final AccountServiceClient accountServiceClient;
+
+    public void checkTransaction(Map<String, Object> payload){
+        String transactionId = (String)payload.get("transactionId");
+        String accountNumber = (String)payload.get("senderAccountNumber");
+        BigDecimal amount = new BigDecimal(payload.get("amount").toString());
+
+        // Fetch real balance from Account Service
+        BigDecimal senderBalance = accountServiceClient.getBalance(accountNumber);
+
+        log.info("Checking transaction: {} account : {} amount: {} balance: {}",
+                transactionId, accountNumber, amount, senderBalance);
+
+        FraudCheckResult result = performFraudChecks(accountNumber, amount, senderBalance);
+    }
+}
